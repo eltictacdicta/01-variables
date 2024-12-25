29ba7cdc308228190e4f06137fddad7ea45b2554
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2024-12-25 20:34:21 +0100

    explicativas

--- 01-nombres.php
+++ 01-nombres.php
@@ -1,2 +1,5 @@
 <?php
+$currentDate = date(format:'Y-m-d');
+echo "El dia actual es: $currentDate";
+?>
 
---
+++ 03-explicativas.php
@@ -0,0 +1,15 @@
+<?php
+
+$address = [
+    "street" => "Calle Principal",
+    "number" => "42",
+    "city" => "Ciudad de México",
+    "zipCode" => "01000"
+];
+
+echo "Calle: " . $address["street"] . "<br>";
+echo "Número: " . $address["number"] . "<br>";
+echo "Ciudad: " . $address["city"] . "<br>";
+echo "Código Postal: " . $address["zipCode"] . "<br>";
+
+?>
